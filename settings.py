57af003f7ec1be41e89794ae2be7b33ace239c18
Yakov Potapov <potapovjakov@gmail.com>
2025-09-20 16:01:48 +0300

    Add settings.py

---
+++ settings.py
@@ -0,0 +1,13 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_REGION_NAME = os.getenv("AWS_REGION_NAME")
+S3_ENDPOINT_URL = os.getenv("S3_ENDPOINT_URL")
+S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
+
+ML_TOKEN = os.getenv("ML_TOKEN")
